d3e763aa058a1e3f3fa97305251b2b7020860a42
Vương Duy Định <45735740+vuongdinh1112@users.noreply.github.com>
2021-10-09 22:03:23 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,48 @@
+//BÀI A.1
+// let input = prompt("Nhập tên Cách nhau bởi dấu phẩy");
+// let allName = input.split(",");
+// console.log(allName)
+// let max = 0;
+
+// for(i = 0;i<allName.length; i++) {
+//   if(max < allName[i].length){
+//       max = allName[i].length
+//   }
+// }
+// console.log(max)
+// let maxLength = []
+// for(i = 0;i<allName.length; i++){
+//     if(allName[i].length>=max) {
+//         maxLength.push(allName[i])
+//     }
+// }
+// console.log(maxLength)
+
+//BÀI A.2
+// let input = prompt("Nhap can nang(cach nhau boi dau \",\")")
+// let weight = input.split(",")
+// let newWeight = []
+// for(i = 0;i<weight.length;i++) {
+//     if(!isNaN(Number(weight[i]))){
+//         newWeight.push(weight[i])
+//     }
+// }
+// console.log(newWeight);
+// let even = 0;
+// for(j = 0; j<newWeight.length; j = j + 2) {
+//     even = even + Number(newWeight[j])
+// }
+// console.log(even)
+
+// let odd = 0;
+// for(j = 1; j<newWeight.length; j = j + 2) {
+//     odd = odd + Number(newWeight[j])
+// }
+// console.log(odd)
+
+// let sum = []
+// sum.push(even, odd)
+// console.log(sum)
+
+
+
